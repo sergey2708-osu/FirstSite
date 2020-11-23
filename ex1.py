c73d114b91eace3f6175bbe0b4441f3921381ecf
sergey2708-osu <74913816+sergey2708-osu@users.noreply.github.com>
2020-11-23 23:23:31 +0300

    Add files via upload

--- ex1.py
+++ ex1.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+import os
 from flask import *
 
 app = Flask(__name__)
@@ -112,4 +112,5 @@ def get_number(number):
                 </html>'''
 
 if __name__ == '__main__':
-    app.run(port=8080, host='127.0.0.1')
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
